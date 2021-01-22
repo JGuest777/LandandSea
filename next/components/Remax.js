a72953a44a3617f69e39ed3ac6acf5db9859ea47
james <jguest777@gmail.com>
2021-01-22 15:15:01 -0500

    remax mini placed & sized

--- next/components/Remax.js
+++ next/components/Remax.js
@@ -1,7 +1,9 @@
 export default function Remax() {
 	return (
 		<div className='remax'>
+			<span>A proud</span>
 			<img src={require('../public/images/logo-remax.png')} />
+			<span>team.</span>
 		</div>
 	)
 }
