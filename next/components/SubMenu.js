cd9fc6d3ee051072724f9cd67b115fb1409a0455
james <jguest777@gmail.com>
2021-01-22 14:07:15 -0500

    fixed submenu layout responsiveness

--- next/components/SubMenu.js
+++ next/components/SubMenu.js
@@ -2,19 +2,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function SubMenu() {
 	return (
-		<>
-			<div className='header__submenu'>
-				<a href='tel:+13214272948' className='hide-desktop'>
-					<FontAwesomeIcon className='header__icons' icon='mobile-alt' />
+		<div className='submenu'>
+			<div className='submenu__mobile hide-desktop'>
+				<a href='tel:+13214272948'>
+					<FontAwesomeIcon className='submenu__icons' icon='mobile-alt' />
 				</a>
-				<a href='mailto:barbara@remax.net' className='hide-desktop'>
-					<FontAwesomeIcon className='header__icons' icon='envelope' />
-				</a>
-				<a href='tel:+13214272948' id='desktop-phone' className='hide-mobile'>
-					<FontAwesomeIcon className='header__icons' icon='phone' size='sm' />
-					(321) 427-2948
+				<a href='mailto:barbara@remax.net'>
+					<FontAwesomeIcon className='submenu__icons' icon='envelope' />
 				</a>
 			</div>
-		</>
+			<div className='submenu__desktop hide-mobile'>
+				<a href='tel:+13214272948' id='desktop-phone'>
+					<FontAwesomeIcon className='submenu__icons' icon='phone' />
+				</a>
+				<p>(321) 427-2948</p>
+			</div>
+		</div>
 	)
 }
