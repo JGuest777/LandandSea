af196b8d42e623c48ba8d66de9ab0a4f4124889b
james <jguest777@gmail.com>
2021-01-27 17:53:01 -0500

    mui button prod test

--- next/components/Menu.js
+++ next/components/Menu.js
@@ -7,7 +7,17 @@ import { withStyles } from '@material-ui/core/styles'
 const styles = (theme) => ({
 	button: {
 		color: 'white',
-		width: '100%'
+		width: '100%',
+		fontSize: '1.25rem'
+	},
+	'&:hover': {
+		backgroundColor: 'red',
+		borderColor: '#0062cc'
+	},
+	'&:active': {
+		boxShadow: 'none',
+		backgroundColor: '#0062cc',
+		borderColor: '#005cbf'
 	},
 	input: {
 		display: 'none'
@@ -33,9 +43,15 @@ function Menu({ classes, links, toggleMenu, setToggleMenu, setToggleOverlay }) {
 		(link) =>
 			link && (
 				<div style={{ textAlign: 'center' }}>
-					<Button size='large' className={classes.button}>
-						<Link href={`/${link.toLowerCase()}`} key={uuidv4()}>
-							{link}
+					<Button fullWidth='true' className={classes.button}>
+						<Link
+							style={{ width: '100%' }}
+							href={`/${link.toLowerCase()}`}
+							key={uuidv4()}
+						>
+							<a ref={menuRef} style={{ width: '100%' }}>
+								{link}
+							</a>
 						</Link>
 					</Button>
 				</div>
