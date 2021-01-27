b21c0cebd04dca5d0d5091a9dfe2b14e1179bf71
james <jguest777@gmail.com>
2021-01-27 17:20:09 -0500

    trying MUI buttons for mobile menu active state

--- next/components/Menu.js
+++ next/components/Menu.js
@@ -1,33 +1,46 @@
-import { useEffect, useRef } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import Link from 'next/link';
+import { useEffect, useRef } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import Link from 'next/link'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
 
-export default function Menu({
-  links,
-  toggleMenu,
-  setToggleMenu,
-  setToggleOverlay,
-}) {
-  const menuRef = useRef();
+const styles = (theme) => ({
+	button: {
+		color: 'white',
+		width: '100%'
+	},
+	input: {
+		display: 'none'
+	}
+})
 
-  useEffect(() => {
-    if (!toggleMenu) return;
-    function handleCLick(e) {
-      if (menuRef.current && !menuRef.current.contains(e.target)) {
-        setToggleMenu(false);
-        setToggleOverlay(false);
-      }
-    }
-    window.addEventListener('click', handleCLick);
-    return () => window.removeEventListener('click', handleCLick);
-  }, [toggleMenu]);
+function Menu({ classes, links, toggleMenu, setToggleMenu, setToggleOverlay }) {
+	const menuRef = useRef()
 
-  return links.map(
-    (link) =>
-      link && (
-        <Link href={`/${link.toLowerCase()}`} key={uuidv4()}>
-          <a ref={menuRef}>{link}</a>
-        </Link>
-      )
-  );
+	useEffect(() => {
+		if (!toggleMenu) return
+		function handleCLick(e) {
+			if (menuRef.current && !menuRef.current.contains(e.target)) {
+				setToggleMenu(false)
+				setToggleOverlay(false)
+			}
+		}
+		window.addEventListener('click', handleCLick)
+		return () => window.removeEventListener('click', handleCLick)
+	}, [toggleMenu])
+
+	return links.map(
+		(link) =>
+			link && (
+				<div style={{ textAlign: 'center' }}>
+					<Link href={`/${link.toLowerCase()}`} key={uuidv4()}>
+						<Button size='large' className={classes.button}>
+							{link}
+						</Button>
+					</Link>
+				</div>
+			)
+	)
 }
+
+export default withStyles(styles)(Menu)
